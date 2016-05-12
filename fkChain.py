9a0a4d491ce9991494b422097a473d863716812e
pietroponti <pietroponti@gmail.com>
2016-05-12 10:13:02 +0100

    Update fkChain.py

--- fkChain.py
+++ fkChain.py
@@ -6,10 +6,17 @@ a = 1
 b = 0
 c = 0
 
+
 for child in scene:
     if 'chain_bone' in child.name():
         bones.append(child)
-        
+
+#Alterante mode using List comprehension)        
+#bones = [child for child in scene if 'chain_bone' in child.name() ]
+
+#Alternate mode to count up
+#for i in range(len(bones)):
+    
 for bone in bones:
     newNull = hou.node('/obj/tentacleRIG1').createNode('null','nullBone1')
     newNull.parm('tz').setExpression('-ch("../chain_bone'+str(a)+'/length")')
@@ -35,4 +42,3 @@ blends.pop()
 for bone in bones:
     bone.setInput(0,blends[c],0)
     c += 1
-    
