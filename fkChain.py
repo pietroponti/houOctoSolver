e8138d70b7251a9848ccba7316cae25adb055d2b
pietroponti <pietroponti@gmail.com>
2016-05-12 19:08:14 +0100

    Update fkChain.py

--- fkChain.py
+++ fkChain.py
@@ -1,9 +1,9 @@
 scene = hou.node('/obj/tentacleRIG1').children()
 bones = []
 nulls = []
-blends = [] 
+fkBones = [] 
 a = 1
-b = 0
+b = 1
 c = 0
 
 
@@ -11,12 +11,6 @@ for child in scene:
     if 'chain_bone' in child.name():
         bones.append(child)
 
-#Alterante mode using List comprehension)        
-#bones = [child for child in scene if 'chain_bone' in child.name() ]
-
-#Alternate mode to count up
-#for i in range(len(bones)):
-    
 for bone in bones:
     newNull = hou.node('/obj/tentacleRIG1').createNode('null','nullBone1')
     newNull.parm('tz').setExpression('-ch("../chain_bone'+str(a)+'/length")')
@@ -25,20 +19,22 @@ for bone in bones:
     a += 1   
     
 for null in nulls:
-    newBlend = hou.node('/obj/tentacleRIG1').createNode('blend','blendK1')
-    newBlend.setInput(0,null,0)
-    blends.append(newBlend)
-    
-    
-for blend in blends:
-    blend.setInput(1,bones[b],0)
+    fkBone = hou.node('/obj/tentacleRIG1').createNode('bone','fkBone1')
+    fkBone.parm('length').setExpression('ch("../chain_bone'+str(b)+'/length")')
+    fkBone.setInput(0,null,0)
+    fkBones.append(fkBone)
     b += 1
     
     
-bones.pop(0)
-blends.pop()
-
-
-for bone in bones:
-    bone.setInput(0,blends[c],0)
-    c += 1
+#for blend in blends:
+#    blend.setInput(1,bones[b],0)
+#    b += 1
+#    
+#    
+#bones.pop(0)
+#blends.pop()
+#
+#
+#for bone in bones:
+#    bone.setInput(0,blends[c],0)
+#    c += 1
